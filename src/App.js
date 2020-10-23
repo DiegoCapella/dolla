21e5e8fe90b566d879593974b7fa76fe4ac4217e
DiegoCapella <diegocapelladev@gmail.com>
2020-10-22 22:08:01 -0300

    Create Navbar & MenuIcon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, World</h1>
-    </div>
+    <Router >
+      <Navbar />
+    </Router >
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const Navbar = () => {
+  return (
+    <>
+      <S.Nav>
+        <S.NavbarContainer>
+          <S.NavLogo to="/">dolla</S.NavLogo>
+          <S.MobileIcon>
+            <S.Bars />
+          </S.MobileIcon>
+          <S.NavMenu>
+            <S.NavItem>
+              <S.NavLinks to="/about">About</S.NavLinks>
+            </S.NavItem>
+            <S.NavItem>
+              <S.NavLinks to="/discover">Discover</S.NavLinks>
+            </S.NavItem>
+            <S.NavItem>
+              <S.NavLinks to="/services">Services</S.NavLinks>
+            </S.NavItem>
+            <S.NavItem>
+              <S.NavLinks to="/signup">Sign Up</S.NavLinks>
+            </S.NavItem>
+          </S.NavMenu>
+          <S.NavBtn>
+            <S.NavBtnLink to="/signin">Sign In</S.NavBtnLink>
+          </S.NavBtn>
+        </S.NavbarContainer>
+      </S.Nav>
+    </>
+  )
+}
+
+export default Navbar
