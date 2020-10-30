e873d0373c68943a381636c807c2c5c5ac6cd82d
DiegoCapella <diegocapelladev@gmail.com>
2020-10-30 20:25:30 -0300

    Update links router

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -11,10 +11,10 @@ const Sidebar = ({ isOpen, toggle }) => {
         </S.Icon>
         <S.SidebarWrapper>
           <S.SidebarMenu>
-            <S.SidebarLink to="/about" onClick={toggle}>About</S.SidebarLink>
-            <S.SidebarLink to="/discover" onClick={toggle}>Discover</S.SidebarLink>
-            <S.SidebarLink to="/services" onClick={toggle}>Services</S.SidebarLink>
-            <S.SidebarLink to="/signup" onClick={toggle}>Sign Up</S.SidebarLink>
+            <S.SidebarLink to="about" onClick={toggle}>About</S.SidebarLink>
+            <S.SidebarLink to="discover" onClick={toggle}>Discover</S.SidebarLink>
+            <S.SidebarLink to="services" onClick={toggle}>Services</S.SidebarLink>
+            <S.SidebarLink to="signup" onClick={toggle}>Sign Up</S.SidebarLink>
           </S.SidebarMenu>
           <S.SideBtnWrap>
             <S.SidebarRoute to="/signin">Sign In</S.SidebarRoute>
